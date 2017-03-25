56b2c3e6c361015e5cd82d22f745a0a1b914c8a1
cangumeli <cgumeli@ku.edu.tr>
2017-03-25 19:56:13 +0300

    Minor UX improvement

--- js/Profile/ProfileSettings.js
+++ js/Profile/ProfileSettings.js
@@ -1,7 +1,8 @@
 import React, {Component, PropTypes} from 'react'
 import {
 	View,
-	TextInput
+	TextInput,
+	Text
 } from 'react-native'
 import { Button } from 'react-native-elements'
 import styles from '../Login/styles'
@@ -12,40 +13,65 @@ class ProfileSettings extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			nameEdited: false,
+			nameEdited: 0,
 			name: ''
 		}
 	}
 	//_send
 	_updateInfo() {
-		//if(this.props.data.loading) {
-			this.props.updateInfo({
-				name: this.state.name
-			})
-		//}
+
+		this.props.updateInfo({
+			name: this.state.name
+		})
+		this.props.data.refetch()
+	}
+
+	_renderSuccess() {
+		try {
+			console.log('render ', this.state.name, this.props.data.viewer.profile.name)
+			if ((this.state.name === this.props.data.viewer.profile.name) && this.state.nameEdited &&
+				(this.state.name !== this.state.initName || (this.nameEdited > 2))) {
+				//this.setState({...this.state, nameEdited:0})
+				return (<Text style={{marginLeft:10}}>Succeed...</Text>)
+			}
+		} catch (err) {
+			console.log(err)
+			return null
+		}
+		return null
+	}
+
+	_getProfileData() {
+		return this.props
+			&& this.props.data
+			&& this.props.data.viewer
+			&& this.props.data.viewer.profile
+			&& this.props.data.viewer.profile.name
+	}
+
+	_storeInitial() {
+		this.setState({...this.state, initName: this._getProfileData()})
 	}
 
 	render() {
+		console.log(this.state.nameEdited)
 		return (
 			<View>
 				<TextInput
 					placeholder='Name'
-					onChangeText={(text) => this.setState({...this.state, name: text, nameEdited:true})}
+					onChangeText={(text) => this.setState({...this.state, name: text, nameEdited:this.state.nameEdited+1})}
 					style={styles.textInput}
 					autoFocus={true}
 					autoCapitalize={'words'}
-					defaultValue={this.props
-						&& this.props.data
-						&& this.props.data.viewer
-						&& this.props.data.viewer.profile
-						&& this.props.data.viewer.profile.name
-					}
-				/>
+					defaultValue={this._getProfileData()}
+					onFocus={this._storeInitial.bind(this)}
+				></TextInput>
+				{this._renderSuccess()}
 				<Button
 					title='Update'
-					buttonStyle={{backgroundColor: '#ff4b48', marginTop:10}}
+					buttonStyle={{backgroundColor: '#0fac0e', marginTop:10}}
 					onPress={this._updateInfo.bind(this)}
-					disabled={!this.state.nameEdited}
+					disabled={this.state.nameEdited === 0}
 				/>
 			</View>
 		)
